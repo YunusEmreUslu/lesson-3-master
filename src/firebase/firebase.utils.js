0710d232bbbaf2c76cf75fe0e3462af551ac6543
YunusEmreUslu <usluyunusemre00@gmail.com>
2022-03-08 19:19:52 +0300

    added stripe integration

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -5,15 +5,14 @@ import "firebase/compat/auth";
 //any forked projets from github with firebase configs must be replaced with this key from this point on
 //The config object is how firebase knows that your application is connected to your firebase account and database!
 const config = {
-  apiKey: "AIzaSyAWLzjdbh9OyMuTjsj0VafgthxwyxM8hTk",
-  authDomain: "crwn-db-bd405.firebaseapp.com",
-  projectId: "crwn-db-bd405",
-  storageBucket: "crwn-db-bd405.appspot.com",
-  messagingSenderId: "852652775038",
-  appId: "1:852652775038:web:097df63fccec5ee59e2a2f",
-  measurementId: "G-LRK62J0Q9B"
+  apiKey: "AIzaSyDYHta_iCDt46u1_gl41WzKIO8lCBSxuS8",
+  authDomain: "crown-db-49b9f.firebaseapp.com",
+  projectId: "crown-db-49b9f",
+  storageBucket: "crown-db-49b9f.appspot.com",
+  messagingSenderId: "336138565900",
+  appId: "1:336138565900:web:ac448596ac3a64383ff129",
+  measurementId: "G-FDB817H128"
 };
-
 //async-await is a more readable way of writing asysnronous code then 
 //".then()" chaining, try catch may be used to catch the errors
 export const createUserProfileDocument = async (userAuth, additionalData) => {
